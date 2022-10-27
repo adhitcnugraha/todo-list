fefd8c40f72540b10ea16a4087a62c0b0da99b6b
Adhitia Cahya Nugraha <adhit.cnugraha@gmail.com>
2022-10-27 17:41:45 +0700

    All done!

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+  return (
+    <div className="container">
+      <div>Todolist Assignment</div>
+      <div>Yarn-React</div>
+    </div>
+  );
+}
+export default Header;
